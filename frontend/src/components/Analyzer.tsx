8177f0ec392768c2c4ddd4f3c29e446bc14d5341
Nagy Ádám <nagada88@gmail.com>
2025-05-17 10:44:03 +0200

    Updated Analyzer.tsx with signal choose functionality

--- frontend/src/components/Analyzer.tsx
+++ frontend/src/components/Analyzer.tsx
@@ -7,6 +7,9 @@ const Analyzer: React.FC = () => {
     const navigate = useNavigate();
     const signals = location.state?.signals;
 
+    const [xsignal, setXsignal] = useState(signals[0]);
+    const [selectedYSignals, setSelectedYSignals] = useState([]);
+    const [plotData, setPlotData] = useState(null);
     useEffect(() => {
         if (!signals) {
             navigate('/');
@@ -18,8 +21,30 @@ const Analyzer: React.FC = () => {
         return null;
     }
 
+   const handleChange = (event) => {
+    setXsignal(event.target.value);
+    }; 
+
     return (
         <div>
+             <div>
+                <label>
+                Mi legyen az x tengelyen?<br/>
+                <select value={xsignal} onChange={handleChange}>
+
+                {signals.map((signal, index) => (
+                    <option key={index} value={signal}>
+                    {signal}
+                    </option>
+                ))}
+
+                </select>
+
+                </label>
+
+                <p>We eat {xsignal}!</p>
+
+            </div>
             <h3>Szignálok:</h3>
             <ul>
                 {signals.map((signal: string) => (
